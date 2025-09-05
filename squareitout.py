ada3038333871b0b1212a263effee8531415d1cd
Mayra30345 <palmayra966@gmail.com>
2025-09-05 18:22:44 +0530

    Add files via upload

---
+++ squareitout.py
@@ -0,0 +1,16 @@
+def SquaredValues(beg,end):
+    lst=[]
+    for i in range (beg,end):
+        lst.append(i**2)
+    lst_even=[]
+    lst_odd=[]
+    for i in lst:
+        if i%2==0:
+            lst_even.append(i)
+        else:
+            lst_odd.append(i)
+    print("Heres a list of even squares within specified range",lst_even)
+    print("Heres a list of odd squares within specified range",lst_odd)
+beg=int(input("Enter a starting range:  "))
+end=int(input("Enter a end range:  "))
+SquaredValues(beg,end)
